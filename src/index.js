8c43a4da2596e58d221f1b90c45390b411333ff7
Sara <sarakirving@gmail.com>
2020-12-12 13:31:20 -0800

    add examples of higher order functions that take a function as an argument

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ function sayHello() {
 
 let fn = sayHello();
 let message = fn();
+
+//higher order functions take a function as an argument, or return a function, or both!
+
+//map is a higher order function 
+let numbers = [1,2,3];
+numbers.map(number => number * 2);
+
+//setTimeout is also a higher order function because it takes a function as an argument
