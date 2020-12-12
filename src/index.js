d80674b7eeefd9c3682f075160502b679787c15a
Sara <sarakirving@gmail.com>
2020-12-12 13:35:23 -0800

    example of functional composition

--- src/index.js
+++ src/index.js
@@ -16,3 +16,14 @@ let numbers = [1,2,3];
 numbers.map(number => number * 2);
 
 //setTimeout is also a higher order function because it takes a function as an argument
+
+//build small functions which you can use to solve a problem
+
+let input = " Javascript ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+
+const result = wrapInDiv(trim(input)); //in functional programming this is called function composition
+
