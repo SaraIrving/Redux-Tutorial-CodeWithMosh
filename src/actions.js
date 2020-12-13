ac2cb278f20a7b1d3aa036fa8898e0022471077d
Sara <sarakirving@gmail.com>
2020-12-12 18:28:39 -0800

    add export statements to arrow functions

--- src/actions.js
+++ src/actions.js
@@ -11,9 +11,16 @@ import * as actions from './actionTypes';
 // }
 
 // alternate syntax using arrow functions 
-const bugAdded = description => ({
+export const bugAdded = description => ({
   type: actions.BUG_ADDED,
       payload: {
-        description: "Bug1"
+        description: description
       }
+})
+
+export const bugRemoved = (id) => ({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: id
+  }
 })
