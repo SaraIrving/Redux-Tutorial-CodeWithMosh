a3592f425b031ad4d70f7890658bb72bb08fc985
Sara <sarakirving@gmail.com>
2020-12-12 18:21:15 -0800

    refactor to create function with arrow functions instead

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,19 @@
 import * as actions from './actionTypes';
 
-export function bugAdded (description) {
-  return {
-    type: actions.BUG_ADDED,
-    payload: {
-      description: "Bug1"
-    }
-  }
+// export function bugAdded (description) {
+//   return {
+//     type: actions.BUG_ADDED,
+//     payload: {
+//       description: "Bug1"
+//     }
+//   }
 
-}
+// }
+
+// alternate syntax using arrow functions 
+const bugAdded = description => ({
+  type: actions.BUG_ADDED,
+      payload: {
+        description: "Bug1"
+      }
+})
